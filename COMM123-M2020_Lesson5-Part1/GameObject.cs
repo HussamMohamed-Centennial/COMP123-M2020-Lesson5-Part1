61a905aa98b79ea83cfa739cdbfef90f13a8c5eb
Hussam Eldin Mohamed <dr.hussam@live.com>
2020-06-22 11:28:23 -0400

    added GameObject Class

---
+++ COMM123-M2020_Lesson5-Part1/GameObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMM123_M2020_Lesson5_Part1
+{
+    class GameObject
+    {
+
+        //PRIVATE INSTANCE MEMBERS(FIELDS)
+        private string m_name;
+        //PUBLIC PROP
+        public Transform Transform { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return m_name;
+            }
+
+            set
+            {
+                m_name = value;
+            }
+        }
+        //CONSTRUCTOR(S)
+        public GameObject(string name)
+        {
+            Name = name;
+        }
+
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+
+    }
+}
